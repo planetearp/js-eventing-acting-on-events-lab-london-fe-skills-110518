97a0fe625a466aa19e65feed09f4a6c8a92c43b8
Thiago de Sa Earp Pagliarelli <thiagoearp@gmail.com>
2018-12-17 20:10:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function moveDodgerLeft() {
 function moveDodgerRight() {
   let rightNumbers = dodger.style.right.replace('px', '')
   let right = parseInt(rightNumbers, 10)
- 
+
   if (right > 0) {
     dodger.style.right = `${right - 1}px`
   }
