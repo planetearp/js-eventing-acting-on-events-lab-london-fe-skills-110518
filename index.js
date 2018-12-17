6b0dbf86875f63b9cb3056ea72ea619a598fa122
Thiago de Sa Earp Pagliarelli <thiagoearp@gmail.com>
2018-12-17 20:11:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ function moveDodgerRight() {
   let right = parseInt(rightNumbers, 10)
 
   if (right > 0) {
-    dodger.style.right = `${right - 1}px`
+    dodger.style.right = `${left + 1}px`
   }
 }
 
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     moveDodgerLeft()
+  } elseif (e.which === 39); {
+    moveDodgerRight();
   }
 })
