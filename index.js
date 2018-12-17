aef7acc2f72c3d199873068ff4805f7b2dd88af9
Thiago de Sa Earp Pagliarelli <thiagoearp@gmail.com>
2018-12-17 20:26:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+const dodger = document.getElementById('dodger')
+
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace('px', '')
+  let left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace('px', '')
+  let left = parseInt(leftNumbers, 10)
+  
+  if (left < 360) {
+    dodger.style.left = `${left + 5}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    moveDodgerRight();
+  }
+})
